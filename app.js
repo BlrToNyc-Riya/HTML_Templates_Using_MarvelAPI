38c493d6c9960bf1357784896941aff4bf6c90d7
Riya Kharbanda <kharbanda.riya.97@gmail.com>
2021-11-22 14:33:09 -0500

    app.js changes

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const static = express.static(__dirname + '/public');
+// const server = require('./server');  
 
 const configRoutes = require('./routes');
 const exphbs = require('express-handlebars');
@@ -13,8 +14,17 @@ app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
 configRoutes(app);
-
-app.listen(3000, () => {
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
   console.log("We've now got a server!");
-  console.log('Your routes will be running on http://localhost:3000');
+  console.log(`Your routes will be running on http://localhost:${port}`);
 });
+
+
+
+// server.create()
+//     .then(app => {
+//         app.listen(port, () => {
+//             console.log(`Server has started on port ${port}!`);
+//         });
+//     }).catch(err => console.log(err));
